e395ad17ec00eca853b46fc7ae0be61cdbd54204
Adhapureddy mahesh <maheshadhapureddy1216@gmail.com>
2026-07-28 03:54:02 +0530

    Remove WeServeYouBetter component from AboutPage
    Removed WeServeYouBetter component from AboutPage.

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,7 +3,6 @@ import AboutHero from "../components/about/AboutHero";
 import WhoWeAre from "../components/about/WhoWeAre";
 import MissionVisionValues from "../components/about/MissionVisionValues";
 // import AboutStatsBar from "../components/about/AboutStatsBar";
-import WeServeYouBetter from "../components/about/WeServeYouBetter";
 // import Testimonials from "../components/about/Testimonials";
 import { CTA } from "../components/home/CTA";
 import Review from "../components/home/Review";
@@ -18,7 +17,7 @@ export default function AboutPage() {
         <WhoWeAre />
         <MissionVisionValues />
    
-        <WeServeYouBetter />
+   
         <Review />
         <div className="mb-6">
         <CTA />
